035ddc5ce4d2386e18c119e16fa795231ccb8aa3
dezssvk <dscr1518@gmail.com>
2023-05-20 17:27:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus ipsum expedita voluptates alias unde vitae ab nostrum eveniet a dolor, molestias autem, harum quaerat. Rerum, sint aut. Nemo, deserunt nostrum?</p>
+    <ul>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero reprehenderit accusamus consectetur illum, voluptas enim officia et, id dignissimos animi, rem voluptatibus totam natus ipsam praesentium laboriosam ex deleniti fuga.</li>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero reprehenderit accusamus consectetur illum, voluptas enim officia et, id dignissimos animi, rem voluptatibus totam natus ipsam praesentium laboriosam ex deleniti fuga.</li>
+        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero reprehenderit accusamus consectetur illum, voluptas enim officia et, id dignissimos animi, rem voluptatibus totam natus ipsam praesentium laboriosam ex deleniti fuga.</li>
+    </ul>
 </body>
 </html>
